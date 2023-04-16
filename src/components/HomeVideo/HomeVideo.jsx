07d426b7cdf906adb8b4e4378be3d2c4fd7a8445
Ali <alicryptovolley@gmail.com>
2023-04-17 02:26:43 +0430

    home post size debugged again

--- src/components/HomeVideo/HomeVideo.jsx
+++ src/components/HomeVideo/HomeVideo.jsx
@@ -2,21 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 const HomeVideo = ({ videoSource }) => {
-   return <Wrapper src={`https://djangoinsta.pythonanywhere.com${videoSource}`} preload="metadata" />;
+   return (
+      <Wrapper>
+         <Video src={`https://djangoinsta.pythonanywhere.com${videoSource}`} preload="metadata" />
+      </Wrapper>
+   );
 };
 
 export default HomeVideo;
 
-const Wrapper = styled.video`
+const Wrapper = styled.div`
    position: relative;
    width: 100%;
    height: 100%;
    display: flex;
    justify-content: center;
    align-items: center;
-   border-radius: 0.5rem;
-
    * {
       fill: ${({ templateTheme }) => templateTheme};
    }
+`;
+
+const Video = styled.video`
+   max-width: 100%;
+   max-height: 100%;
+   object-fit: contain;
+   object-position: center center;
+   border-radius: 0.5rem;
 `;
